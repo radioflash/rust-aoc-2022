e8c9d7514de5c8362153e293e858d271bcb59790
Wolfgang Puppx <wolfgang.pupp@gmail.com>
2023-02-14 00:25:11 +0100

    Extend API and command line parsing for part 2

--- src/d1.rs
+++ src/d1.rs
@@ -1,15 +1,26 @@
-pub fn solve(input: String) -> i32 {
-    return input
-        .split("\n\n") // split into per-elf chunks
-        .map(|s| {
-            s.split("\n").fold(0, |acc, x| {
-                match x {
-                    "" => acc,
-                    _ => acc + x.parse::<i32>().expect("invalid calorie number")
-                }
-            })
-        }) // sum calories for each elf
-        .max().expect("invalid input: no maximum"); // max value
+pub fn solve(input: String, part: i32) -> i32 {
+    let calories_per_elf = input
+    .split("\n\n") // split into per-elf chunks
+    .map(|s| {
+        s.split("\n").fold(0, |acc, x| {
+            match x {
+                "" => acc,
+                _ => acc + x.parse::<i32>().expect("invalid calorie number")
+            }
+        })
+    }); // sum calories for each elf
+
+    match part {
+        1 => calories_per_elf.max().expect("Got no maximum value, invalid input?"),
+        2 => {
+            let mut list: Vec<i32> = calories_per_elf.collect::<Vec<i32>>(); // collect entries
+            list.sort_by(|a, b| b.cmp(a)); // get entries in reverse order
+            list.truncate(3); // truncate to top 3
+            list.iter().sum()
+        },
+        _ => panic!("Invalid Part")
+    }
+
 }
 
 #[cfg(test)]
@@ -17,25 +28,30 @@ mod tests {
     use super::*;
     use indoc::indoc;
 
+    const s: String = indoc! {"
+        1000
+        2000
+        3000
+
+        4000
+
+        5000
+        6000
+
+        7000
+        8000
+        9000
+
+        10000
+    "};
+
     #[test]
-    fn check() {
-        let s = indoc! {"
-            1000
-            2000
-            3000
-
-            4000
-
-            5000
-            6000
-
-            7000
-            8000
-            9000
-
-            10000
-        "};
+    fn part1() {
         assert!(!s.contains(" "));
-        assert_eq!(solve(s.to_string()), 24000);
+        assert_eq!(solve(s.to_string(), 1), 24000);
+    }
+    fn part2() {
+        assert!(!s.contains(" "));
+        assert_eq!(solve(s.to_string(), 2), 45000);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,46 @@
+use anyhow::Context;
+use indoc::indoc;
+
 mod d1;
 mod input_fetcher;
 
-fn solve_exercise(i: i32) {
+fn solve_exercise(exercise: i32, part: i32) {
     let input = input_fetcher::fetch(1, "53616c7465645f5fd2b00d1351048ded6ed85148811c17bfd2f81729e928fd838a035b378a7ec97f6b223b4e21c90c68330f08675a72a503bb8969ba2af31cfe");
-    match i {
-        1 => println!("{}", d1::solve(input)),
-        _ => println!("Exercise {} NYI", i),
+    match exercise {
+        1 => println!("{}", d1::solve(input, part)),
+        _ => println!("Exercise {} NYI", exercise),
     }
 }
 
 fn main() {
+    let arg1 = std::env::args().nth(1);
+    let arg2 = std::env::args().nth(2);
+
+    if arg1.is_none() || arg2.is_none() {
+        println!(indoc! {"Run with 'cargo run <exercise> <part>' where:
+            <exercise> is in [1-25]
+            <part> is either 1 or 2
+        "});
+    }
+
     let exercise = match std::env::args().nth(1) {
-        Some(s) => s.parse::<i32>().expect("Parsing failed"),
+        Some(s) => s.parse::<i32>().with_context(|| format!("Failed to parse first argument, got \"{}\", expected an integer in  [1, 25]", s)).expect("Parse error"),
         None => 1,
     };
+    let part = match std::env::args().nth(2) {
+        Some(s) => s.parse::<i32>().with_context(|| format!("Failed to parse first argument, got \"{}\", expected an integer in  [1, 25]", s)).expect("Parse error"),
+        None => 1,
+    };
+
     assert!(
         exercise >= 1 && exercise <= 25,
         "Invalid exercise number; 1-25 is allowed"
     );
+    assert!(
+        part >= 1 && part <= 2,
+        "Invalid part number; 1-2 is allowed"
+    );
 
-    println!("Solution for: {}", exercise);
-    solve_exercise(exercise);
+    println!("Solution for exercise {}, part {}", exercise, part);
+    solve_exercise(exercise, part);
 }
