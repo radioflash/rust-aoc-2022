d37f5d5365b650839418001b560bb0c18d0ee232
Wolfgang Puppx <wolfgang.pupp@gmail.com>
2023-02-14 22:13:17 +0100

    Add part 2

--- src/exercises/d2.rs
+++ src/exercises/d2.rs
@@ -1,12 +1,28 @@
 use super::Part;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum Shape {
     Rock,
     Paper,
     Scissors
 }
 use Shape::{Rock, Paper, Scissors};
+
+fn pick_to_lose(opp: Shape) -> Shape {
+    match opp {
+        Rock => Scissors,
+        Paper => Rock,
+        Scissors => Paper,
+    }
+}
+
+fn pick_to_win(opp: Shape) -> Shape {
+    match opp {
+        Rock => Paper,
+        Paper => Scissors,
+        Scissors => Rock,
+    }
+}
 
 fn score(opp: Shape, us: Shape) -> i32 {
     match (us, opp) { // reverse us/opponent for readability
@@ -24,35 +40,40 @@ fn score(opp: Shape, us: Shape) -> i32 {
 
 pub fn solve(part: Part, input: impl Into<String>) -> i32 {
 
-    match part {
-        Part::One => {
-            let s = input.into();
-            let mut sum: i32 = 0;
-            for l in s.split('\n') {
-                if l.is_empty() {
-                    break;
-                }
+
+    let s = input.into();
+    let mut sum: i32 = 0;
+    for l in s.split('\n') {
+        if l.is_empty() {
+            break;
+        }
+
+        let mut moves = l.split(' ');
+        let opp = match moves.nth(0).expect("expected opponent move") {
+            "A" => Rock,
+            "B" => Paper,
+            "C" => Scissors,
+            invalid_mode => panic!("invalid move {}, expected A, B or C", invalid_mode),
+        };
         
-                let mut moves = l.split(' ');
-                let opp = match moves.nth(0).expect("expected opponent move") {
-                    "A" => Rock,
-                    "B" => Paper,
-                    "C" => Scissors,
-                    invalid_mode => panic!("invalid move {}, expected A, B or C", invalid_mode),
-                };
-                let us = match moves.nth(0).expect("expected our move") {
-                    "X" => Rock,
-                    "Y" => Paper,
-                    "Z" => Scissors,
-                    invalid_mode => panic!("invalid move {}, expected X, Y or Z", invalid_mode),
-                };
-        
-                sum += score(opp, us);        
-            }
-            return sum
-        },
-        Part::Two => todo!(),
+        let us = match part {
+            Part::One => match moves.nth(0).expect("expected our move") {
+                "X" => Rock,
+                "Y" => Paper,
+                "Z" => Scissors,
+                invalid_mode => panic!("invalid move {}, expected X, Y or Z", invalid_mode),
+            },
+            Part::Two => match moves.nth(0).expect("expected our move") {
+                "X" => pick_to_lose(opp),
+                "Y" => opp,
+                "Z" => pick_to_win(opp),
+                invalid_mode => panic!("invalid move {}, expected X, Y or Z", invalid_mode),
+            },
+        };
+
+        sum += score(opp, us);        
     }
+    return sum
 }
 
 #[cfg(test)]
@@ -70,8 +91,9 @@ mod tests {
     fn part1() {
         assert_eq!(solve(Part::One, TEST_INPUT), 15);
     }
+
     #[test]
     fn part2() {
-        //assert_eq!(solve(TEST_INPUT.to_string(), Part::One), 45000);
+        assert_eq!(solve(Part::Two, TEST_INPUT), 12);
     }
 }
