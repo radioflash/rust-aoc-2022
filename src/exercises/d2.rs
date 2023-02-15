277b26047b59135534a56d150c9f3aa92ea59307
Wolfgang Puppx <wolfgang.pupp@gmail.com>
2023-02-15 01:55:05 +0100

    Minor cleanup and refactoring

--- src/exercises/d2.rs
+++ src/exercises/d2.rs
@@ -9,8 +9,8 @@ enum Shape {
 use Shape::{Paper, Rock, Scissors};
 
 // note that this is decrement (mod 3)
-const fn pick_to_lose(opp: Shape) -> Shape {
-    match opp {
+const fn pick_to_lose(opponents_pick: Shape) -> Shape {
+    match opponents_pick {
         Rock => Scissors,
         Paper => Rock,
         Scissors => Paper,
@@ -27,34 +27,40 @@ const fn pick_to_win(opponents_pick: Shape) -> Shape {
 }
 
 // score component from winning or losing
-fn winscore(opponents_pick: Shape, our_pick: Shape) -> i32 {
-    if our_pick == pick_to_win(opponents_pick) {
-        return 6;
-    } else if our_pick == pick_to_lose(opponents_pick) {
-        return 0;
+fn win_score(opponents_pick: Shape, our_pick: Shape) -> i32 {
+    if our_pick == opponents_pick {
+        return 3; // draw
+    } else if our_pick == pick_to_win(opponents_pick) {
+        return 6; // win
     }
-    return 3;
+    return 0; // loss
+}
+
+// score that we get for picking a certain shape
+fn shape_score(our_pick: Shape) -> i32 {
+    match our_pick {
+        Rock => 1,
+        Paper => 2,
+        Scissors => 3,
+    }
 }
 
 // total score including the "value" or our pick
-fn total_score(opponents_pick: Shape, our_pick: Shape) -> i32 {
-    match (opponents_pick, our_pick) {
-        (opp, Rock) => 1 + winscore(opp, Rock),
-        (opp, Paper) => 2 + winscore(opp, Paper),
-        (opp, Scissors) => 3 + winscore(opp, Scissors),
-    }
+fn score(opponents_pick: Shape, our_pick: Shape) -> i32 {
+    return shape_score(our_pick) + win_score(opponents_pick, our_pick);
 }
 
 pub fn solve(part: Part, input: impl Into<String>) -> i32 {
     let s = input.into();
+
     let mut sum: i32 = 0;
     for l in s.split('\n') {
         if l.is_empty() {
-            break;
+            continue; //skip empty line which may occur at the end
         }
 
         let mut moves = l.split(' ');
-        let opponents_pick = match moves.nth(0).expect("expected opponent move") {
+        let opponents_pick = match moves.next().expect("expected opponent move") {
             "A" => Rock,
             "B" => Paper,
             "C" => Scissors,
@@ -62,13 +68,13 @@ pub fn solve(part: Part, input: impl Into<String>) -> i32 {
         };
 
         let our_pick = match part {
-            Part::One => match moves.nth(0).expect("expected our move") {
+            Part::One => match moves.next().expect("expected our move") {
                 "X" => Rock,
                 "Y" => Paper,
                 "Z" => Scissors,
                 invalid_mode => panic!("invalid move {}, expected X, Y or Z", invalid_mode),
             },
-            Part::Two => match moves.nth(0).expect("expected our move") {
+            Part::Two => match moves.next().expect("expected our move") {
                 "X" => pick_to_lose(opponents_pick),
                 "Y" => opponents_pick,
                 "Z" => pick_to_win(opponents_pick),
@@ -76,7 +82,7 @@ pub fn solve(part: Part, input: impl Into<String>) -> i32 {
             },
         };
 
-        sum += total_score(opponents_pick, our_pick);
+        sum += score(opponents_pick, our_pick);
     }
     return sum;
 }
